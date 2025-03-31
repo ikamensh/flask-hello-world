723899665055358d11b1cdf2a2ea179536544f7b
ikamen <ikamenshchikov@gmail.com>
2025-03-31 15:35:59 +0200

    test

--- api/index.py
+++ api/index.py
@@ -1,8 +1,16 @@
 from flask import Flask, Response
 from pydantic import BaseModel
+import sys
+from pathlib import Path
+import time
 
-# from mock_package.compliance import msg
-msg = "LOCAL MSG"
+api = Path(__file__).parent
+root = api.parent
+
+sys.path.append(str(root) )
+
+from mock_package.compliance import msg
+# msg = "LOCAL MSG"
 
 app = Flask(__name__)
 
@@ -16,6 +24,7 @@ class MyPydanticObject(BaseModel):
 
 @app.route('/about')
 def about():
+    time.sleep(3)
     my_obj = MyPydanticObject(field1=123, field2=f"{msg}")
     json_data = my_obj.model_dump_json() # Use .json() for Pydantic V1
     return Response(json_data, mimetype='application/json')
